83aad980d5072a40b028db8d93f744ec4ab1b899
Diego Garzon Molina <g.molina.diego@gmail.com>
2018-02-22 14:01:51 +0100

    JavaScript async, clases y objectos complejos

---
+++ JS/async.js
@@ -0,0 +1,32 @@
+
+
+//JavaScript no es multihilo, pero si asíncrono
+//Asincronía
+//Eventos de usuario
+//LLamadas a servidor
+//Temporización
+
+//funciones de temporización
+var contador = 0
+function hazEsto(){
+    console.log(contador)
+    ++contador
+}
+setTimeout(hazEsto,1000)
+/*
+setInterval(()=>{console.log(contador)
+    ++contador
+},1000)*/
+
+
+//En ES6
+
+const consulta = function(){
+    return new Promise ( (resolve,reject) => {
+    setTimeout(()=>{
+       resolve('Todo ok')
+        },1000)
+})
+}
+let resultados = 
+consulta ().then((res)=>{console.log(res)})
--- JS/clases.js
+++ JS/clases.js
@@ -3,5 +3,40 @@
 
 
 class User{
+   constructor (nombre,apellidos,edad,genero){
+        this.nombre = nombre
+        this.apellidos = apellidos
+        this.edad = edad
+        this.genero = genero
+        ++ User.prototype.numUser
+   }
+        saludar(){
+            console.log(`Hola marinero, ya somos: ${this.numUser}`)
+        }
     
 }
+
+User.prototype.numUser = 0
+class Alumno extends User{
+    constructor (nombre,apellidos,edad,genero,curso){
+       super(nombre,apellidos,edad,genero)
+       this.curso = curso
+      
+    }
+    saludar(){
+        console.log("Hola estudiante, ya somos: " + this.numUser)
+    }
+}
+oAlumno = new Alumno ('Juan','Lopez','23','H','Angular')
+
+/*User.prototype.saludar = function(){
+    console.log(`Hola marinero, ya somos: ${this.numUser}`)
+}*/
+
+user = new User ('Juan','Lopez','23','H')
+console.log(user)
+user.saludar()
+oAlumno.saludar()
+console.log(oAlumno)
+
+//console.log(user.prototype.numUser)
