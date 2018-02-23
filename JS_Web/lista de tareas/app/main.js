48b6fc1e6b696e5e3f516a7de2b9695f04767275
Diego Garzon Molina <g.molina.diego@gmail.com>
2018-02-23 13:45:36 +0100

    Acceso a elementos de html a través de js

--- JS_Web/lista de tareas/app/main.js
+++ JS_Web/lista de tareas/app/main.js
@@ -7,10 +7,23 @@ class Main{
            inputTarea: document.getElementById('tarea'),
            resultado: document.getElementById('resultado'),
            arrayTareas: new Array()
-       }
-       this.modelo.botonRegistrar.addEventListener('click',this.btnEventRegistrar.bind(this),false)
-       this.modelo.botonAñadir.addEventListener('click',this.btnEventAñadir.bind(this),false)
+        }
+        this._rellenarNombre()
+        this._rellenarTareas()
+        this.modelo.botonRegistrar.addEventListener('click',this.btnEventRegistrar.bind(this),false)
+        this.modelo.botonAñadir.addEventListener('click',this.btnEventAñadir.bind(this),false)
       
+    }
+    _rellenarNombre(){
+        var nombre = localStorage.getItem('nombre')
+        if (nombre) this.modelo.resultado.innerHTML='<p id="parrafoRespuesta"><strong>'+nombre+', la respuesta es 42 con tareas:</strong><ul id="lista"></ul></p>'
+        this.modelo.resultado.classList.add('verde')
+    }
+    _rellenarTareas(){
+        this.modelo.arrayTareas = JSON.parse(localStorage.getItem('tareas')) ? JSON.parse(localStorage.getItem('tareas')) : []
+        this.modelo.arrayTareas.forEach(element => {
+            document.getElementById('lista').innerHTML+='<li>'+element+'</li>'
+        });
     }
 
      btnEventRegistrar(){
@@ -18,14 +31,16 @@ class Main{
        // user = window.prompt('Dime tu nombre','Pepe')   
         var nombre = this.modelo.inputNombre.value
         var tarea = this.modelo.inputTarea.value
-        this.modelo.resultado.innerHTML='<p id="parrafoRespuesta" ><strong>'+nombre+', la respuesta es 42 con tareas:</strong></p><ul id="lista"></ul>'
-        this.modelo.resultado.classList.toggle('verde')
+        localStorage.setItem('nombre',nombre)
+        this.modelo.resultado.innerHTML='<p id="parrafoRespuesta" ><strong>'+nombre+', la respuesta es 42 con tareas:</strong><ul id="lista"></ul></p>'
+        this.modelo.resultado.classList.add('verde')
    //  document.getElementById('parrafoRespuesta').className+=' verde'
     }
     btnEventAñadir(){
         var tarea = this.modelo.inputTarea.value
         this.modelo.arrayTareas.push(tarea)
         document.getElementById('lista').innerHTML+='<li>'+tarea+'</li>'
+        localStorage.setItem('tareas',JSON.stringify(this.modelo.arrayTareas))
     }
    
 }
