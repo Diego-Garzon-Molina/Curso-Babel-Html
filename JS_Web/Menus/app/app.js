6cfa9f104e199bd5d9bd3af504aeb7fb4986ca05
Diego Garzon Molina <g.molina.diego@gmail.com>
2018-02-27 10:32:52 +0100

    Templates, inicio Proyecto HANDS-ON HTML-CSS-JS

--- JS_Web/Menus/app/app.js
+++ JS_Web/Menus/app/app.js
@@ -3,5 +3,4 @@ import { Main } from './main.js'
 
 (function(){
    document.addEventListener('DOMContentLoaded',() => new Main(),false) 
-    console.log('Hola mundo ')
 })()
--- JS_Web/Menus/app/main.js
+++ JS_Web/Menus/app/main.js
@@ -3,15 +3,31 @@ export class Main {
         console.log('Main loaded')
         this.vista = {
             aBtnsMenu: document.querySelectorAll('a'),
-            eArticle : document.querySelector('article')
+            eArticle : document.querySelector('article'),
+            body : document.querySelector('body'),
+            eMain : document.querySelector('main'),
+            aImports: document.querySelectorAll('link[rel="import"]'),
+            oImports: {}
         }
         this.vista.aBtnsMenu.forEach(element => {
             element.addEventListener('click',this.menuItems.bind(this),false)
         });
-        this.vista.eArticle.innerHTML = '<h2>'+'Inicio'+'</h2>'
+        this.vista.eMain.innerHTML = '<h2>'+'Inicio'+'</h2>'
+        this.vista.aImports.forEach(elem=>{
+            this.vista.oImports[elem.title]=elem.import
+        })
     }
     menuItems(oEv){
+      
+        this._cargarTemplate(oEv.target.id)
         oEv.preventDefault()
-        this.vista.eArticle.innerHTML = '<h2>'+oEv.target.id    +'</h2>'
+          }
+    _cargarTemplate(id){
+        console.log(this.vista.oImports)
+        console.log(id)
+        const IMPORT = this.vista.oImports[id]
+        console.log(IMPORT)
+        const ELEM = IMPORT.querySelector(`#${id}`)
+        this.vista.eMain.innerHTML = ELEM.innerHTML
     }
 }
